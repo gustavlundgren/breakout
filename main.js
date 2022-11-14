70b8941d0a26aa55e1bf72d6c9097c515e90f134
Gustav Lundgren <sgvln005@edu.karlstad.com>
2022-11-14 19:48:15 +0100

    Kollision mellan boll och spelare

--- main.js
+++ main.js
@@ -16,12 +16,29 @@ class Game{
         this.range = range
 
         this.player = new Player(this, range.value)
+        this.balls = []
+        this.ballInterval = 300
+        this.ballTimer = 0
     }
     update(){
         this.player.update()
+
+        if(this.ballTimer > this.ballInterval){
+            this.balls.push(new Ball(this.player, this))
+            this.ballTimer = 0
+        }else{
+            this.ballTimer++
+        }
+
+        if(this.balls.length == 2){
+            this.balls.pop()
+        }
+
+        this.balls.forEach(object => object.update())
     }
     draw(){
         this.player.draw(this.ctx)
+        this.balls.forEach(object => object.draw(this.ctx))
     }
 }
 
@@ -43,15 +60,53 @@ class Player{
     }
 }
 
-class Ball{
-    constructor(){
 
+//skapa en vektorer
+class Ball{
+    constructor(player, game){
+        this.game = game
+        this.player = player
+
+        this.size = 5
+        this.x = this.player.x + this.player.width/ 2
+        this.y = this.player.y - this.size/ 2
+    
+        this.xVel = 1
+        this.yVel = -1
     }
     update(){
 
-    }
-    draw(){
+        if(this.x < this.size / 2){
+            this.xVel = -this.xVel
+        }
 
+        if(this.x > (this.game.width) - (this.size / 2)){
+            this.xVel = -this.xVel
+        }
+
+        if(this.y < this.size/ 2){
+            this.yVel = -this.yVel
+        }
+
+        //Förlora liv 
+        if(this.y > this.game.height + this.size/ 2){
+            this.y = this.player.y - this.size/ 2
+            this.x = this.player.x + this.player.width/ 2
+            this.yVel = -this.yVel
+        }
+
+        //kolla efter kollisoin med spelaren
+        if(this.y + this.size / 2 > this.player.y && this.x > this.player.x && this.x < this.player.x + this.player.width){
+            this.yVel = -this.yVel
+        }
+
+        this.x += this.xVel
+        this.y += this.yVel
+    }
+    draw(ctx){
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI)
+        ctx.fill()
     }
 }
 
@@ -67,7 +122,6 @@ function main(){
     game.update()
     game.draw()
 
-    //ctx.fillRect(20, 20, 100, 100)
     requestAnimationFrame(main)
 }
 
